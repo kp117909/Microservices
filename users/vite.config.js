ea0d5ad21e56c89618fc3eb8a95665392c087302
Kamil <kpolak491@gmail.com>
2025-03-06 12:36:27 +0100

    working vite + laravel on docker

---
+++ users/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 5173,
+            host: 'localhost',
+        },
+        port: 5173,
+        watch: {
+            usePolling: true,
+            pollInterval: 1000  
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
